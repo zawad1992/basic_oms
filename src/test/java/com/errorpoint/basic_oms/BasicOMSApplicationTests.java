c984a5ffdb3e4b363d9529c78df5d7968f1ef360
Zawadul Kawum <zawad1992@gmail.com>
2024-09-09 16:00:41 +0600

    Basic Test Case for OMS

---
+++ src/test/java/com/errorpoint/basic_oms/BasicOMSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.errorpoint.basic_oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicOMSApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
