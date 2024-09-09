b0690c468c3afc415993a92b6ee287e24746a57b
Zawadul Kawum <zawad1992@gmail.com>
2024-09-09 15:58:49 +0600

    Main Class and application.properties configuration

---
+++ src/main/java/com/errorpoint/basic_oms/BasicOMSApplication.java
@@ -0,0 +1,13 @@
+package com.errorpoint.basic_oms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicOMSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicOMSApplication.class, args);
+    }
+
+}
