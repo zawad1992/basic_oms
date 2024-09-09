27463d9d82607f46dda67618d062886f0e3901f8
Zawadul Kawum <zawad1992@gmail.com>
2024-09-09 15:59:56 +0600

    Customer Repository Interface for ORM Facility

---
+++ src/main/java/com/errorpoint/basic_oms/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.errorpoint.basic_oms.repository;
+
+import com.errorpoint.basic_oms.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+}
